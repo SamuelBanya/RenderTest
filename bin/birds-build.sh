100bb2102957d0f7b5d0773481700d59d7c1e8db
Samuel Banya <sbanya@fastmail.com>
2022-12-19 18:10:20 -0600

    Adding show action

--- bin/birds-build.sh
+++ bin/birds-build.sh
@@ -7,4 +7,6 @@ bundle install
 # bundle exec rake assets:precompile
 # bundle exec rake assets:clean
 bundle exec rake db:migrate
-bundle exec rake db:seed
+# NOTE:
+# Commented out the following seed command to prevent duplicate seed actions:
+# bundle exec rake db:seed
